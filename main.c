6a181d372849b95fd54d857d1d31c9bdf82c1ea5
lzx <20235983@neu.edu.cn>
2024-04-15 21:42:21 +0800

    feat: test sample

--- main.c
+++ main.c
@@ -13,15 +13,52 @@
 #include <stdio.h>
 #include "include/database.h"
 int main() {
-    db new = database_connect("new.db");
-    map newmap = map_create();
-    map_set(&newmap, "newtable",16);
-    map_set(&newmap, "what", 9);
-    printf("%d\n", map_get(&newmap, "newtable"));
-    printf("%d\n", map_get(&newmap, "what"));
-    database_insert_table(&new, "test1", newmap);
-    list newlist = list_create(sizeof(char));
-    list_append(&newlist, "123");
-    database_insert_line(&new, "newtable", newlist);
+    db the = database_connect("data.txt");
+
+    map status = map_create();
+    map_set(&status, "username", 16); // 16 is the max length of the
+    map_set(&status, "password", 16); // username and password
+    map_set(&status, "auth", 6); // auth token
+
+    database_insert_table(&the, "status", status);
+    map_free(&status);
+
+    map user_info = map_create();
+    map_set(&user_info, "username", 16);
+    map_set(&user_info, "phone", 16);
+    map_set(&user_info, "zone", 24);
+    //向数据库中插入新表（table类型）（user_info）
+    database_insert_table(&the, "user_info", user_info);
+    map_free(&user_info);
+    list users = list_create(sizeof(char*));
+    char* string1 = "what";
+    char* string2 = "114514";
+    char* string3 = "19d19h";
+    list_append(&users, string1);
+    list_append(&users, string2);
+    list_append(&users, string3);
+    database_insert_line(&the, "user_info", users);
+    list_free(&users);
+
+    map fee = map_create();
+    map_set(&fee, "username", 16);
+    map_set(&fee, "fee", 16);
+    map_set(&fee, "paid", 16);
+
+    database_insert_table(&the, "fee", fee);
+    map_free(&fee);
+
+    map log = map_create();
+    map_set(&log, "username", 16);
+    map_set(&log, "time", 16);
+    map_set(&log, "action", 16);
+    map_set(&log, "amount", 16);
+
+    database_insert_table(&the, "log", log);
+    map_free(&log);
+
+    printf("%s", database_select(&the, "user_info", "phone", 1));
+    database_remove_table(&the, "log");
+    /*database_vacuum(&the);*/
     return 0;
 }
