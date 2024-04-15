4c2e3f7dcee15d601ae0a3a6be23b42b2f52fa1d
Wu Junkai <wu.junkai@qq.com>
2024-04-15 13:14:51 +0800

    chore: 添加了对Cmake的完整支持

--- main.c
+++ main.c
@@ -3,49 +3,32 @@
 #include "base_type.h"
 
 #include "database.h"
+
 #include "auth.h"
 
-#include "user_admin.h"
-#include "user_worker.h"
-#include "user_resident.h"
-
-#include "UI.h"
+#include <gtk/gtk.h>
 
 void login(str, str);
 
 int main() {
+    gtk_init(NULL, NULL);
     // set windows
-    console_utf8();
-    console_clear();
-    console_size(100, 80);
-    console_title("Property Manager");
-    // login
-    char username[16]={}, password[16]={};
-    while(true){
-        login(username, password);
-        auth account = auth_create(username, password);
-        if(auth_is_login(account)){
-            break;
-        }
-    }
-    // load index page
+    login("username", "password");
 }
 
 void login(str username, str password){
-    console_new();
-    
-    ui_input input_name = ui_input_register();
-    ui_input_set(&input_name, username, {7,  8}, 20);
-    ui_input input_word = ui_input_register();
-    ui_input_set(&input_word, password, {12, 8}, 20);
-    
-    ui_button clear_button = ui_button_register();
-    ui_button_set(&clear_button, "clear", {17,  8}, 8);
-    ui_button_operate(&clear_button, UI_CLEAR_INPUT);
+    GtkBuilder *builder;
+    GObject *window;
 
-    ui_button check_button = ui_button_register();
-    ui_button_set(&check_button, "login", {17, 18}, 8);
-    ui_button_operate(&check_button, UI_EXIT);
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "C:/Users/wujun/Desktop/propert/assets/login.ui", NULL);
 
-    return console_mainloop();
+    window  = gtk_builder_get_object (builder, "window");
+
+    g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+    //show the window
+    gtk_widget_show_all (GTK_WIDGET(window));
+
+    gtk_main ();
 }
