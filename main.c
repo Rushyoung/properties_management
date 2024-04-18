38588e717cc428ee3384cda4a4e17828bd87d7aa
WuJunkai2004 <wujunkai20041123@outlook.com>
2024-04-18 21:50:25 +0800

    feat: 合并初始化函数到主程序

--- main.c
+++ main.c
@@ -10,6 +10,39 @@
 
 void login(str, str);
 
+void init_create_database(){
+    db goal = db_connect("data.db");
+
+    map status = map_init( {"username", 16},
+                           {"password", 16},
+                           {"auth", 6} );
+    db_insert_table(&goal, "status", status);
+    map_free(&status);
+
+    map user_info = map_init( {"username", 16},
+                              {"phone", 16},
+                              {"zone", 24} );
+    db_insert_table(&goal, "user_info", user_info);
+    map_free(&user_info);
+
+    map fee = map_init( {"username", 16},
+                        {"fee", 16},
+                        {"paid", 16} );
+    db_insert_table(&goal, "fee", fee);
+    map_free(&fee);
+
+    map log = map_init( {"username", 16},
+                        {"time", 16},
+                        {"action", 16},
+                        {"amount", 16} );
+    db_insert_table(&goal, "log", log);
+    map_free(&log);
+
+    list admin = list_init(string("admin"), string("ttakods"), string("0"));
+    db_insert_lin(&goal, "status", admin);
+    list_free(&admin);
+}
+
 int main() {
     gtk_init(NULL, NULL);
     // set windows
