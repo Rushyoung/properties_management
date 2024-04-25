0f4e5f1af7c7b3d7f38c002e92a259faf182f2da
joker <2822343002@qq.com>
2024-04-25 14:53:36 +0800

    feat: 新增了输入字符数限制

--- src/residient_ui.c
+++ src/residient_ui.c
@@ -6,7 +6,11 @@
 #include <windows.h>
 #include "../include/ui.h"
 
-
+void change_window(GtkWidget *widget, gpointer data){
+    GtkWidget *window = data;
+    widget_destory(window, NULL);
+    resident_main(0, NULL);
+}
 
 int resident_main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
@@ -16,6 +20,7 @@ int resident_main(int argc, char *argv[]) {
     gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
 
     GtkWidget *table = gtk_table_new(10, 20, FALSE);
     gtk_container_add(GTK_CONTAINER(window), table);
@@ -67,15 +72,171 @@ int resident_main(int argc, char *argv[]) {
 
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_menu_item), file_menu);
     gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file_menu_item);
-    gtk_table_attach(GTK_TABLE(table), menubar, 8, 9, 1, 2,GTK_FILL|GTK_EXPAND,GTK_FILL|GTK_EXPAND, 3, 3);
+    gtk_table_attach(GTK_TABLE(table), menubar, 8, 9, 3, 4,GTK_FILL|GTK_EXPAND,GTK_FILL|GTK_EXPAND, 3, 3);
 
     GtkWidget *lable = gtk_label_new("当前页面：业主界面");
-    gtk_table_attach(GTK_TABLE(table), lable, 8, 9, 0, 1,GTK_FILL|GTK_EXPAND,GTK_FILL|GTK_EXPAND, 3, 3);
+    gtk_table_attach(GTK_TABLE(table), lable, 8, 9, 5, 7,GTK_FILL|GTK_EXPAND,GTK_FILL|GTK_EXPAND, 3, 3);
     gtk_label_set_justify(GTK_LABEL(lable), GTK_JUSTIFY_LEFT);
 
-    GtkWidget *button = gtk_button_new_with_label("返回上一级");
-    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(destroy_window_callback), window);
-    gtk_table_attach(GTK_TABLE(table), button, 8, 9, 2, 3,
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
+
+int submit(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
+    gtk_window_set_default_size(GTK_WINDOW(window), 60, 40);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    GtkWidget *table = gtk_table_new(5, 5, FALSE);
+    gtk_container_add(GTK_CONTAINER(window), table);
+
+    gtk_table_set_row_spacings(GTK_TABLE(table), 30);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 30);
+
+    GtkWidget *label = gtk_label_new("收款码：");
+    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT);
+
+    GtkWidget *image = gtk_image_new_from_file("../asset/qrcode.png");
+    gtk_table_attach(GTK_TABLE(table), image, 1, 4, 1, 5,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_label_set_justify(GTK_LABEL(image), GTK_JUSTIFY_LEFT);
+
+    GtkWidget *button1 = gtk_button_new_with_label("确定");
+    g_signal_connect(button1, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    gtk_table_attach(GTK_TABLE(table), button1, 1, 2, 5, 6,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_label_set_justify(GTK_LABEL(button1), GTK_JUSTIFY_LEFT);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
+
+int resident_fee(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    //  创建主窗口
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *table = gtk_table_new(10, 20, FALSE);
+    gtk_container_add(GTK_CONTAINER(window), table);
+
+    gtk_table_set_row_spacings(GTK_TABLE(table), 30);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 30);
+
+    GtkWidget *image = gtk_image_new_from_file("../asset/logo2.png");
+    gtk_table_attach(GTK_TABLE(table), image, 0, 5, 0, 4,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 3, 3);
+
+    GtkWidget *label = gtk_label_new("当前页面：用户界面-费用缴纳");
+    gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT);
+    gtk_table_attach(GTK_TABLE(table), label, 1, 2, 5, 6,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button1 = gtk_button_new_with_label("返回上一级");
+    g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(change_window), window);
+    gtk_table_attach(GTK_TABLE(table), button1, 1, 2, 6, 7,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button2 = gtk_button_new_with_label("缴费");
+    g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(submit), NULL);
+    gtk_table_attach(GTK_TABLE(table), button2, 4, 15, 2, 3,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button3 = gtk_button_new_with_label("缴费记录");
+    g_signal_connect(G_OBJECT(button3), "clicked", G_CALLBACK(submit), NULL);
+    gtk_table_attach(GTK_TABLE(table), button3, 4, 15, 3, 4,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
+
+int _main(int argc, char *argv[]){
+    gtk_init(&argc, &argv);
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
+    gtk_window_set_default_size(GTK_WINDOW(window), 60, 40);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    GtkWidget *table = gtk_table_new(4, 4, FALSE);
+    gtk_container_add(GTK_CONTAINER(window), table);
+
+    gtk_table_set_row_spacings(GTK_TABLE(table), 30);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 30);
+
+    GtkWidget *lable = gtk_label_new("故障描述");
+    gtk_table_attach(GTK_TABLE(table), lable, 0, 1, 1, 2,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_label_set_justify(GTK_LABEL(lable), GTK_JUSTIFY_LEFT);
+
+    GtkWidget *entry = gtk_entry_new();
+    gtk_entry_set_width_chars(GTK_ENTRY(entry), 32);
+    gtk_table_attach(GTK_TABLE(table), entry, 1, 2, 1, 2,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_entry_set_max_length(GTK_ENTRY(entry), 20);
+
+    GtkWidget *button1 = gtk_button_new_with_label("确定");
+    g_signal_connect(button1, "clicked", G_CALLBACK(gtk_widget_destroy), NULL);
+    gtk_table_attach(GTK_TABLE(table), button1, 1, 2, 3, 4,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+    gtk_label_set_justify(GTK_LABEL(button1), GTK_JUSTIFY_LEFT);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
+int resident(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    //  创建主窗口
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    GtkWidget *table = gtk_table_new(10, 20, FALSE);
+    gtk_container_add(GTK_CONTAINER(window), table);
+
+    gtk_table_set_row_spacings(GTK_TABLE(table), 30);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 30);
+
+    GtkWidget *image = gtk_image_new_from_file("../asset/logo2.png");
+    gtk_table_attach(GTK_TABLE(table), image, 0, 5, 0, 4,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 3, 3);
+
+    GtkWidget *label = gtk_label_new("当前页面：用户界面-问题上报");
+    gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT);
+    gtk_table_attach(GTK_TABLE(table), label, 1, 2, 5, 6,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button1 = gtk_button_new_with_label("返回上一级");
+    g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(change_window), window);
+    gtk_table_attach(GTK_TABLE(table), button1, 1, 2, 6, 7,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button2 = gtk_button_new_with_label("上报");
+    g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(submit), NULL);
+    gtk_table_attach(GTK_TABLE(table), button2, 4, 15, 2, 3,
+                     GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
+
+    GtkWidget *button3 = gtk_button_new_with_label("上报记录");
+    g_signal_connect(G_OBJECT(button3), "clicked", G_CALLBACK(submit), NULL);
+    gtk_table_attach(GTK_TABLE(table), button3, 4, 15, 3, 4,
                      GTK_FILL | GTK_EXPAND, GTK_FILL | GTK_EXPAND, 0, 0);
 
     gtk_widget_show_all(window);
