c9ff7e2946d2279b8901458e169384d6c78160b0
joker <2822343002@qq.com>
2024-04-26 02:02:37 +0800

    feat: 加了几个函数

--- src/ui_functions.c
+++ src/ui_functions.c
@@ -111,3 +111,13 @@ char* get_time(){
     sprintf(time, "%d%02d%02d%02d%02d", tmp->tm_year + 1900, tmp->tm_mon + 1, tmp->tm_mday, tmp->tm_hour, tmp->tm_min);
     return time;
 }
+
+int check_time(char* a){
+    int temp = atoi(a);
+    if(temp % 1000000 > 152359){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
