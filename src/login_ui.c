7da510d5679dd37f15c7edf83e700b298d084b51
joker <2822343002@qq.com>
2024-04-27 16:53:01 +0800

    feat: 随便搞搞

--- src/login_ui.c
+++ src/login_ui.c
@@ -55,7 +55,6 @@ int login_main(int argc, char *argv[]) {
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "世界树物业管理系统");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 100);
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
