bc579f829570ef2c7178b40653c08d52a5ce5894
Rushyoung <74442033+Rushyoung@users.noreply.github.com>
2024-04-27 13:42:47 +0800

    feat: debug

--- main.c
+++ main.c
@@ -14,36 +14,46 @@
 #include "include/auth.h"
 #include "include/ui.h"
 
+//#define DEBUG
+
 db database;
 
 int main(int argc, char *argv[]) {
     database = init();
-    str string1 = "user1";
-    str string2 = "1919810";
-    str string3 = "liuhao";
-    str string4 = "16";
-    str string5 = "20230201";
-    add_guard(&database, string1, string2, string3, string4, string5);
-    string1 = "mycomputerdad";
-    string2 = "000000";
-    string3 = "mjc";
-    string4 = "01";
-    string5 = "19410614";
-    add_guard(&database, string1, string2, string3, string4, string5);
-    update_password(&database, "mycomputerdad", "114514");
-    list_link_head result = database_qsort(&database, "account", "username");
-    struct list_link_node* cur = result.next;
-    printf("%s\n", list_get(char*, &(cur->data), 1));
-    cur = cur->next;
-    printf("%s\n", list_get(char*, &(cur->data), 1));
-    cur = cur->next;
-    printf("%s\n", list_get(char*, &(cur->data), 1));
-    string1 = "manba";
-    string2 = "000000";
-    string3 = "kobe";
-    string4 = "1d";
-    string5 =  "101";
-    add_residents(&database, string1, string2, string3, string4, string5);
+#ifdef DEBUG
+    char username[20];
+    char password[20];
+    char name[20];
+    char region[20];
+    char room[20];
+    char last_time[20];
+    FILE* resident_fp = fopen("resident.txt", "rb");
+    if(resident_fp != NULL){
+        while(!feof(resident_fp)){
+            fscanf(resident_fp, "%16s %16s %s %s %s %s", username, password, region, room, last_time, name);
+            add_residents(&database, username, password, name, region, room);
+            database_update(&database, "resident", "last_time", database_query_by_column(&database, "resident", "username", username), last_time);
+        }
+    }
+    fclose(resident_fp);
+    FILE* guard_fp = fopen("worker.txt", "rb");
+    if(guard_fp != NULL){
+        while (!feof(guard_fp)){
+            fscanf(guard_fp, "%16s %16s %s %s %s", username, password, name, region, last_time);
+            add_guard(&database, username, password, name, region, last_time);
+        }
+    }
+    fclose(guard_fp);
+
+
+
+#else
     login_main(argc, argv);
+
+
+
+
+#endif
+
     return 0;
 }
